ee511adaeb02dd20e44e39f19ccae8288291630c
Ravik272 <121052832+Ravik272@users.noreply.github.com>
2022-12-20 00:06:17 +0530

    Update streamlit_app.py
    Added header line & some menu data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bluberry Datmeal')
+streamlit.text('Kale,spinach & Rocket smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
