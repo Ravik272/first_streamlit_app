f2de04b2cf6cdf75135df8d874d548a4184415a5
Ravik272 <121052832+Ravik272@users.noreply.github.com>
2022-12-23 12:34:20 +0530

    Update streamlit_app.py
    updated the previous step to see clear view with the fruit load list data instead of snowflake account details

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,9 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+#my_cur.execute("select current_User(), current_account(), current_region()")
+my_cur.execute("select * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The Fruit Load List Contains :")
+# streamlit.text("Hello from Snowflake:")
+streamlit.dataframe(my_data_rows)
