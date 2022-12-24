457dc8020aabe5f2e04f8f5dffe6566ff80522db
Ravik272 <121052832+Ravik272@users.noreply.github.com>
2022-12-24 12:47:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 #fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 #Create a repeatable code block called afunction:
-def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvoice.com/api/fruit/" + this_fruit_choice)
+def get_fruityvice_data(fruit_choice):
+  fruityvice_response = requests.get("https://fruityvoice.com/api/fruit/" + fruit_choice)
   fruityvice_normalized =pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 # New section to display fruityvice api response
@@ -35,7 +35,7 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    back_from_function = get_fruityvice_data(this_fruit_choice)
+    back_from_function = get_fruityvice_data(fruit_choice)
     
     
 #streamlit.write('The user entered ', fruit_choice)
